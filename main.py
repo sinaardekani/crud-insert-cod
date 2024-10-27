7cd6a5885101bf7669217e87f10dde1c19292ab0
iranmanesh <iranmaneshp1971@gmail.com>
2024-10-27 23:11:06 +0330

    closeimage

--- main.py
+++ main.py
@@ -3,13 +3,4 @@ from tkinter import *
 win=Tk()
 
 
-def DestroyWindow(e):
-
-    win.destroy()
 closeImage=PhotoImage(file='images/close-window-16.png')
-
-CloseBtn=Button(win,image=closeImage)
-CloseBtn.bind('<Button-1>',DestroyWindow)
-CloseBtn.place(x=10,y=10)
-
-
