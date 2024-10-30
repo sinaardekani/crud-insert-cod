e5e853165152eb0546f1ba3e751d92053604f30e
sina <sinaardekanis87@gmail.com>
2024-10-30 23:35:52 +0330

    Removed embedded repositories from index

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def DestroyWindow(e):
     #win.destroy()
 closeImage=PhotoImage(file='images/close-window-16.png')
 
-CloseBtn=Button(win,image=closeImage)
-CloseBtn.bind('<Button-1>',DestroyWindow)
-CloseBtn.place(x=10,y=10)
+#CloseBtn=Button(win,image=closeImage)
+#CloseBtn.bind('<Button-1>',DestroyWindow)
+#CloseBtn.place(x=10,y=10)
