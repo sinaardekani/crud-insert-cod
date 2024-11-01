6f0cf0b35999bb5cfae6b3fd13384b3917abcac4
sina <sinaardekanis87@gmail.com>
2024-11-01 19:49:33 +0330

    added

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ from tkinter import messagebox
 win=Tk()
 
 
+
+
 def DestroyWindow(e):
     win.destroy()
     closeImage = PhotoImage(file='images/close-window-16.png')
     CloseBtn = Button(win, image=closeImage)
-    CloseBtn.bind('<Button>', DestroyWindow)
+    CloseBtn.bind('<Button-1>', DestroyWindow)
     CloseBtn.place(x=15, y=15)
-
-
-
-
-
-
 
     # image
     #closeImage = PhotoImage(file='images/close-window-16.png')
