ba31a2da4d9370f07cac354e43b3492e3bc8280f
sina <sinaardekanis87@gmail.com>
2024-11-01 16:05:03 +0330

    how work a button

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def DestroyWindow(e):
     win.destroy()
     closeImage = PhotoImage(file='images/close-window-16.png')
     CloseBtn = Button(win, image=closeImage)
-    #CloseBtn.bind('<Button-1>', DestroyWindow)
-    #CloseBtn.place(x=10, y=10)
+    CloseBtn.bind('<Button-1>', DestroyWindow)
+    CloseBtn.place(x=10, y=10)
 
 
 
