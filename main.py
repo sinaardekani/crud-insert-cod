1c8d493006bfc5589d4b1cffd5f28691a76ac440
sina <sinaardekanis87@gmail.com>
2024-11-01 16:04:10 +0330

    build a button

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ win=Tk()
 def DestroyWindow(e):
     win.destroy()
     closeImage = PhotoImage(file='images/close-window-16.png')
-    #CloseBtn = Button(win, image=closeImage)
+    CloseBtn = Button(win, image=closeImage)
     #CloseBtn.bind('<Button-1>', DestroyWindow)
     #CloseBtn.place(x=10, y=10)
 
