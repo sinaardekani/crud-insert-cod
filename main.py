cd6d3b99824cc5752a47442ece648ee59816bc5f
sina <sinaardekanis87@gmail.com>
2024-11-01 15:43:43 +0330

    added a screen

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from tkinter import *
-from tkinter import messagebox
+#from tkinter import messagebox
+
+win=Tk()
 
 
 
-def DestroyWindow(e):
-    win.destroy()
-    #win.destroy()
-closeImage=PhotoImage(file='images/close-window-16.png')
 
-CloseBtn=Button(win,image=closeImage)
 
-CloseBtn.bind('<Button-1>',DestroyWindow)
-CloseBtn.place(x=10,y=10)
+
+
+
+
+
+
+
+win.mainloop()
 
 
