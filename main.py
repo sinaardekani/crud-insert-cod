3bc807b1555a0073ca01d9be5efb743d9c9db233
sina <sinaardekanis87@gmail.com>
2024-11-01 16:03:13 +0330

    added a photoimage

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ win=Tk()
 
 def DestroyWindow(e):
     win.destroy()
-    #closeImage = PhotoImage(file='images/close-window-16.png')
+    closeImage = PhotoImage(file='images/close-window-16.png')
     #CloseBtn = Button(win, image=closeImage)
     #CloseBtn.bind('<Button-1>', DestroyWindow)
     #CloseBtn.place(x=10, y=10)
