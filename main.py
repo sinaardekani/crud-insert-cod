c761ce6a42e4fca64d9257c87a760bd9abdbf01a
sina <sinaardekanis87@gmail.com>
2024-11-01 16:02:29 +0330

    added a function

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from tkinter import *
 win=Tk()
 
 
-#def DestroyWindow(e):
-    #win.destroy()
+def DestroyWindow(e):
+    win.destroy()
     #closeImage = PhotoImage(file='images/close-window-16.png')
     #CloseBtn = Button(win, image=closeImage)
     #CloseBtn.bind('<Button-1>', DestroyWindow)
