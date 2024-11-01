3b35cefc83aebdf05921e003cabe6679e895c0d4
sina <sinaardekanis87@gmail.com>
2024-11-01 15:50:53 +0330

    added a function

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from tkinter import *
 win=Tk()
 
 
+def DestroyWindow(e):
+    win.destroy()
+
+#CloseBtn=Button(win,image=closeImage)
+#CloseBtn.bind('<Button-1>',DestroyWindow)
+#CloseBtn.place(x=10,y=10)
 
 
 
